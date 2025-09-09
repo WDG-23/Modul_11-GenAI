47e52dadebb6c28e53bda7e521f9871f751fdcd6
Stephan Ullmann <stephan.ullmann@posteo.de>
2025-09-09 11:52:34 +0200

    genAi starter

---
+++ 01-chat/backend/server.js
@@ -0,0 +1,24 @@
+import cors from 'cors';
+import express from 'express';
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Running' });
+});
+O;
+app.use('/{*splat}', () => {
+  throw Error('Page not found', { cause: { status: 404 } });
+});
+
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  res.status(err.cause?.status || 500).json({ message: err.message });
+});
+
+app.listen(port, () => console.log(`AI Proxy listening on port ${port}`));
